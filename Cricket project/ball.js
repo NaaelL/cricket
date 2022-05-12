64ec550fca661fb8a2a1791c5bc222288a0bcd54
NaaelL <90194739+NaaelL@users.noreply.github.com>
2022-05-12 17:57:29 +0400

    Add files via upload

---
+++ Cricket project/ball.js
@@ -0,0 +1,34 @@
+class Ball {
+
+    constructor (x, y, r) {
+        var options = {
+            isStatic: false,
+            restitution: 0.5,
+            friction: 0,
+            density: 0.5
+        }
+
+        // this.x = x
+        // this.y = y
+        // this.r = r
+
+        this.ballImg = loadImage ("images/ball.png");
+    
+        this.body = Bodies.circle (x, y, r, options);
+        World.add (world, this.body);
+
+    }
+    display () {
+        var pos = this.body.position
+        push ();
+        //translate (pos.x, pos.y)
+        imageMode (CENTER);
+        image (this.ballImg, pos.x, pos.y);
+        pop ();
+
+    }
+   
+
+}
+
+
